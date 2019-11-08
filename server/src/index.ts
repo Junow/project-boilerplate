0481f08d2d3fcb4b4484b65b8bd12b6051c202a9
didi0410 <taristmas@gmail.com>
2019-11-09 03:03:40 +0900

    express

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
